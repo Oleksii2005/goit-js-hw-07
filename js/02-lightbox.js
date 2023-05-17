3743f4eca4fe0daa8a4a1b950b21bf34b17a5d86
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2023-05-17 21:12:34 +0200

    js changes

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,42 +1,81 @@
+// import { galleryItems } from "./gallery-items.js";
+// // Change code below this line
+// const galleryList = document.querySelector(".gallery");
+// const galleryCard = ({ preview, original, description } = {}) => {
+//   return `<li class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//      <img
+//      class="gallery__image"
+//      src="${preview}"
+//      alt="${description}" />
+//   </a>
+// </li>`;
+// };
+// const galleryAr = galleryItems.map((el) => galleryCard(el));
+
+// galleryList.insertAdjacentHTML("afterbegin", galleryAr.join(""));
+
+// // Модальне вікно
+
+// const onImageClick = (event) => {
+//   // відмінна дії за замовчуванням
+//   event.preventDefault();
+
+//   const currentTarget = event.target;
+//   // перевірка на картинку
+
+//   if (currentTarget.nodeName !== "IMG") {
+//     return;
+//   }
+//   const instance = basicLightbox.create(
+//     `<img src ="${currentTarget.dataset.source}" width="800"height="600">`
+//   );
+//   const escapeHandler = (event) => {
+//     if (event.code === "Escape") {
+//       instance.close();
+//       document.removeEventListener("keydown", escapeHandler);
+//     }
+//   };
+//   instance.show();
+//   document.addEventListener("keydown", escapeHandler);
+// };
+// const lightbox = new SimpleLightbox(".gallery a", {
+//   captions: true,
+//   captionPosition: "bottom",
+//   captionsData: "alt",
+//   captionDelay: 250,
+// });
+// galleryList.addEventListener("click", onImageClick);
+
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const galleryList = document.querySelector(".gallery");
-const galleryCard = ({ preview, original, description } = {}) => {
-  return `<li class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-     <img 
-     class="gallery__image" 
-     src="${preview}" 
-     alt="${description}" />
-  </a>
-</li>`;
-};
-const galleryAr = galleryItems.map((el) => galleryCard(el));
+const list = document.querySelector(".gallery");
 
-galleryList.insertAdjacentHTML("afterbegin", galleryAr.join(""));
+const galleryElements = [];
 
-// Модальне вікно
+galleryItems.forEach((galleryItem) => {
+  const item = document.createElement("li");
+  item.classList.add("gallery__item");
 
-const onImageClick = (event) => {
-  // відмінна дії за замовчуванням
-  event.preventDefault();
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.href = galleryItem.original;
 
-  const currentTarget = event.target;
-  // перевірка на картинку
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.src = galleryItem.preview;
+  img.alt = galleryItem.description;
 
-  if (currentTarget.nodeName !== "IMG") {
-    return;
-  }
-  const instance = basicLightbox.create(
-    `<img src ="${currentTarget.dataset.source}" width="800"height="600">`
-  );
-  const escapeHandler = (event) => {
-    if (event.code === "Escape") {
-      instance.close();
-      document.removeEventListener("keydown", escapeHandler);
-    }
-  };
-  instance.show();
-  document.addEventListener("keydown", escapeHandler);
-};
-galleryList.addEventListener("click", onImageClick);
+  item.appendChild(link);
+  link.appendChild(img);
+
+  galleryElements.push(item);
+});
+
+list.append(...galleryElements);
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionPosition: "bottom",
+  captionsData: "alt",
+  captionDelay: 250,
+});
